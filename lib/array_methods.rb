64b74a1c5ce48042e84572f17c30bff81de21e02
Rachid Rakaa <rachid1982fsb@gmail.com>
2019-09-28 22:49:29 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -16,7 +16,9 @@ def find_max_value(array)
   while array[counter] do
     if max<array[counter+1]
       max =array[counter+1]
-      
+    end
+    counter += 1
+    max
 end
 
 def find_min_value(array)
